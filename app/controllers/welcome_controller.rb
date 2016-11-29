b8f3cd2739081727003dfbc410847301f377dc15
rorusage <rorusage@gmail.com>
2016-11-29 17:45:45 +0800

    add notice helper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:info] = "資訊"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def notice_message
+    alert_types = {notice: :success, alert: :danger}
+
+    contents = content_tag(:button, "x" ,class: "close", "data-dismiss" => "alert", "aria-hidden" => "true")
+
+    alerts = flash.map do |type, message|
+      alert_content = contents + message
+
+      alert_type = alert_types[type.to_sym] || type
+      alert_class = "alert alert-#{alert_type} alert-dismissable"
+
+      content_tag(:div, alert_content, class: alert_class)
+    end
+    alerts.join("\n").html_safe
+  end
 end
