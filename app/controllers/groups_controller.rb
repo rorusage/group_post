af161d9e2f239d5b0f5b4e743c0fc9cfde2a9831
rorusage <rorusage@gmail.com>
2016-12-01 15:51:54 +0800

    build edit, update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,11 +22,17 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
 
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit
+    end
   end
 
   private
