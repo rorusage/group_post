221389d64e5ffbad1ab78889c8a331476233e557
rorusage <rorusage@gmail.com>
2016-12-02 16:46:27 +0800

    build post edit, update and dstroy actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "新增修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.destroy
+    redirect_to group_path(@group), alert: "留言刪除成功！"
+  end
+
   private
 
   def post_params
