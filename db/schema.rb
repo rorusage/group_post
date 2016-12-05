8bf5453c0c01a57c19148be45bde47225846b318
rorusage <rorusage@gmail.com>
2016-12-05 16:29:48 +0800

    add name column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202090203) do
+ActiveRecord::Schema.define(version: 20161205082844) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20161202090203) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
