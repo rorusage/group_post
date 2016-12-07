2ba270a729c735ef03383ec13a90aa0990ba081c
rorusage <rorusage@gmail.com>
2016-12-07 16:18:50 +0800

    Only the author can edit or delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,16 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_group
   def new
-    post = @group.posts.new
+    @post = @group.posts.new
   end
 
   def create
     @post = @group.posts.build(post_params)
+    @post.author = current_user
+
+    #上面2行也可以這樣
+    #@post = current_user.posts.new(post_params)
+    #@post.group_id = @group.id
 
     if @post.save
       redirect_to group_path(@group), notice: "新增留言成功！"
@@ -16,11 +21,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to group_path(@group), notice: "新增修改成功！"
@@ -30,7 +35,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     redirect_to group_path(@group), alert: "留言刪除成功！"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205085855) do
+ActiveRecord::Schema.define(version: 20161205091903) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161205085855) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
