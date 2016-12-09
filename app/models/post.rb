a90cabfb500e09156c9710ffa633510fc8a9159a
rorusage <rorusage@gmail.com>
2016-12-09 16:53:29 +0800

    build rake dev:build

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
+  scope :recent, ->{order("updated_at DESC")}
 
   def editable_by?(user)
     user == author
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "這個種子檔案會自動建立一個帳號，並且創建20個groups, 每個group各30個posts"
+
+create_groups = for i in 1..20 do
+                    Group.create!([title: "Group no. #{i}", description: "這是用種子建立的第#{i}個討論版", user_id: "1"])
+                    GroupUser.create(group_id: i, user_id: 1)
+                    for k in 1..30 do
+                      Post.create!([group_id: "#{i}", content: "這是用種子建立的第#{k}個留言", user_id: "1"])
+                    end
+                end
